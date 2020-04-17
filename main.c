6d9490e5386cbd06ed2ba6d21808c02ac23567df
Samuel Rodrigues <samuel.rodrigues@eisti.fr>
2020-04-17 14:11:57 +0200

    Added Documentation

---
+++ main.c
@@ -0,0 +1,14 @@
+/** @file main.c
+ *  @author Samuel Rodrigues <samuel.rodrigues@eisti.eu>
+ *  @version 0.1
+ *  @date Fri 17 Apr 2020 12:14
+ *
+ *  @brief 
+ *
+ */
+
+int main(int argc, char const *argv[])
+{
+
+    return (0);
+}
---
+++ main.h
@@ -0,0 +1,22 @@
+/** @file main.h
+ *  @author Samuel Rodrigues <samuel.rodrigues@eisti.eu>
+ *  @version 0.1
+ *  @date Fri 17 Apr 2020 12:14
+ *
+ *  @brief 
+ *
+ */
+
+/** 
+ *  @author Samuel Rodrigues <samuel.rodrigues@eisti.eu>
+ *  @version 0.1
+ *  @date Fri 17 Apr 2020 13:37
+ *
+ *  @brief Fonction Principale 
+ *
+ *  @param[in] argc
+ *  @param[in] *argv[]
+ *  @return EXIT_SUCCESS : le programme doit se terminer normalement
+ *
+ */
+int main(int argc, char const *argv[]);
